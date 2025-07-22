9c99b4db10fa6367e279cb5395d22006c78fe07e
rohitkshirsagar19 <rohitkshirsagar1904@gmail.com>
2025-07-22 14:29:32 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import '@mysten/dapp-kit/dist/index.css';
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import LoyaltyCardPage from './App';
+
+const queryClient = new QueryClient();
+const networks = {
+  testnet: { url: getFullnodeUrl('testnet') },
+  mainnet: { url: getFullnodeUrl('mainnet') },
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork="testnet">
+        <WalletProvider>
+          <BrowserRouter>
+            <LoyaltyCardPage />
+          </BrowserRouter>
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
